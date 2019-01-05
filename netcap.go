d3684d44242d7e1afe6ba21b766cece6b5da3bc5
Philipp Mieden <dreadl0ck@protonmail.ch>
2019-01-05 14:42:18 +0100

    generated dist

--- netcap.go
+++ netcap.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version is the current version of the netcap library
-const Version = "v0.3.7"
+const Version = "v0.3.8"
 
 // InitRecord initializes a new record of the given type
 // that conforms to the proto.Message interface
