461e3e8027741544cc24050237f61b244a465f80
Philipp Mieden <dreadl0ck@protonmail.ch>
2019-05-03 19:44:44 +0200

    added net.util

---
+++ cmd/util/main.go
@@ -0,0 +1,39 @@
+/*
+ * NETCAP - Traffic Analysis Framework
+ * Copyright (c) 2017 Philipp Mieden <dreadl0ck [at] protonmail [dot] ch>
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/dreadl0ck/netcap/utils"
+)
+
+func main() {
+
+	flag.Parse()
+
+	// util to convert netcap timestamp to UTC time
+	if *flagToUTC != "" {
+		fmt.Println(utils.TimeToUTC(*flagToUTC))
+		os.Exit(1)
+	}
+
+	// util to check if fields count matches for all generated rows
+	if *flagCheckFields {
+		checkFields()
+		return
+	}
+}
