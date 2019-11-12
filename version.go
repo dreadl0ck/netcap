7b9c02e8fad280ffec04f8b80ffb384f55279bbc
Philipp Mieden <dreadl0ck@protonmail.ch>
2019-11-12 22:57:00 +0100

    v0.4.2 release

--- version.go
+++ version.go
@@ -1,5 +1,5 @@
-// This file was generated. Do not edit.
+// Netcap is a framework for network packet processing at the protocol level. Learn more at https://netcap.io.
 package netcap
 
 // Netcap version.
-var Version = "v0.4.1"
+var Version = "v0.4.2"
