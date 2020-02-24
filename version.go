192a8cc560c70ae84665cf578f32923140f14b29
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-02-24 15:21:39 +0100

    version bump

--- version.go
+++ version.go
@@ -2,5 +2,5 @@
 package netcap
 
 // Netcap version.
-var Version = "v0.4.3"
-var Commit = "eb48668bbb9c761d29ee030aebfd77dd8c8e765d"
+var Version = "v0.4.4"
+var Commit = "7231959968f9741562cadd097f36ebf091c4d3b8"
