bd101e4eae557d46629ad3156adb33002681913e
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-03-15 17:46:20 +0100

    v0.4.7 release

--- version.go
+++ version.go
@@ -2,5 +2,5 @@
 package netcap
 
 // Netcap version.
-var Version = "v0.4.6"
-var Commit = "599ced2a8f0f7bd03b626451d0ee7511ae520afc"
+var Version = "v0.4.7"
+var Commit = "6759e53197c4df3fc56d6e769863cbe7fbd35db2"
