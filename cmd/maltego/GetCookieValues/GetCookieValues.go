3603001022d6e8cf0c444430aeabf652c8604e9d
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-03-16 20:19:23 +0100

    implemented GetCookieValues

--- cmd/maltego/GetCookieValues/GetCookieValues.go
+++ cmd/maltego/GetCookieValues/GetCookieValues.go
@@ -9,28 +9,30 @@ func main() {
 	maltego.HTTPTransform(
 		nil,
 		func(lt maltego.LocalTransform, trx *maltego.MaltegoTransform, http *types.HTTP, minPackets, maxPackets uint64, profilesFile string, ipaddr string) {
+			cookieName := lt.Values["properties.httpcookie"]
 			if http.SrcIP == ipaddr {
 				for _, c := range http.ReqCookies {
-					addCookie(trx, c, http.Timestamp, ipaddr, profilesFile)
+					if c.Name == cookieName {
+						addCookieValue(trx, c, http.Timestamp)
+					}
 				}
 				for _, c := range http.ResCookies {
-					addCookie(trx, c, http.Timestamp, ipaddr, profilesFile)
+					if c.Name == cookieName {
+						addCookieValue(trx, c, http.Timestamp)
+					}
 				}
 			}
 		},
 	)
 }
 
-func addCookie(trx *maltego.MaltegoTransform, c *types.HTTPCookie, timestamp string, ipaddr string, profilesFile string) {
-	ent := trx.AddEntity("netcap.HTTPCookie", c.Name)
-	ent.SetType("netcap.HTTPCookie")
-	ent.SetValue(c.Name)
+func addCookieValue(trx *maltego.MaltegoTransform, c *types.HTTPCookie, timestamp string) {
+	ent := trx.AddEntity("netcap.HTTPCookieValue", c.Value)
+	ent.SetType("netcap.HTTPCookieValue")
+	ent.SetValue(c.Value)
 
 	di := "<h3>HTTP Cookie</h3><p>Timestamp: " + timestamp + "</p>"
 	ent.AddDisplayInformation(di, "Netcap Info")
-
-	ent.AddProperty("ipaddr", "IPAddress", "strict", ipaddr)
-	ent.AddProperty("path", "Path", "strict", profilesFile)
 
 	//ent.SetLinkLabel(strconv.FormatInt(dns..NumPackets, 10) + " pkts")
 	ent.SetLinkColor("#000000")
