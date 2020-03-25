45ecdb138c66039c8f953a950a731f3ad0bd9b28
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-03-25 22:15:09 +0100

    added GetFileType

---
+++ cmd/transform/GetFileType.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	maltego "github.com/dreadl0ck/netcap/maltego"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func GetFileType() {
+
+	lt := maltego.ParseLocalArguments(os.Args)
+	trx := &maltego.MaltegoTransform{}
+
+	log.Println("path:", lt.Values["path"])
+
+	out, err := exec.Command("file", "-b", lt.Values["path"]).CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := maltego.EscapeText(string(out))
+	ent := trx.AddEntity("netcap.FileType", name)
+	ent.SetType("netcap.FileType")
+	ent.SetValue(name)
+	ent.SetLinkColor("#000000")
+
+	fmt.Println(trx.ReturnOutput())
+}
