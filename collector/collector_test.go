e5d30b56eb387c1c6da881bbdb59ed9f3d94c0a6
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-04-04 14:38:09 +0200

    added basic tests for reading and writing audit record files

---
+++ collector/collector_test.go
@@ -0,0 +1,62 @@
+package collector
+
+import (
+	"fmt"
+	"github.com/dreadl0ck/netcap"
+	"github.com/dreadl0ck/netcap/encoder"
+	"github.com/dreadl0ck/netcap/resolvers"
+	"github.com/dreadl0ck/netcap/utils"
+	"testing"
+	"time"
+)
+
+func TestCollectPCAP(t *testing.T) {
+
+	// init collector
+	c := New(Config{
+		Live:                false,
+		Workers:             12,
+		PacketBufferSize:    100,
+		WriteUnknownPackets: false,
+		Promisc:             false,
+		SnapLen:             1514,
+		BaseLayer:           utils.GetBaseLayer("ethernet"),
+		DecodeOptions:       utils.GetDecodeOptions("datagrams"),
+		FileStorage:         "files",
+		Quiet:               true,
+		EncoderConfig: encoder.Config{
+			Buffer:          true,
+			Compression:     true,
+			CSV:             false,
+			IncludeEncoders: "",
+			ExcludeEncoders: "",
+			Out:             "../tests/collector-test",
+			Source:          "unit tests",
+			Version:         netcap.Version,
+			IncludePayloads: false,
+			Export:          false,
+			AddContext:      true,
+			MemBufferSize:   1024*1024*10,
+		},
+		ResolverConfig: resolvers.Config{
+			ReverseDNS:      false,
+			LocalDNS:        false,
+			MACDB:           true,
+			Ja3DB:           true,
+			ServiceDB:       true,
+			GeolocationDB:   true,
+		},
+		DPI: false,
+	})
+
+	c.PrintConfiguration()
+
+	// start timer
+	start := time.Now()
+
+	if err := c.CollectPcapNG("../tests/The-Ultimate-PCAP-v20200224.pcapng"); err != nil {
+		t.Fatal("failed to collect audit records from pcapng file: ", err)
+	}
+
+	fmt.Println("done in", time.Since(start))
+}
