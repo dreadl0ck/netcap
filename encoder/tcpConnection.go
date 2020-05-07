738b2c91e866cab95dc6ff398c401dba8eb6e345
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-05-07 18:41:25 +0200

    moved tcpFactory into separate file

--- encoder/tcpConnection.go
+++ encoder/tcpConnection.go
@@ -54,13 +54,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"runtime/pprof"
 	"strconv"
 	"sync"
 	"time"
 
-	"github.com/dreadl0ck/gopacket/ip4defrag"
 	"github.com/evilsocket/islazy/tui"
 
 	"github.com/dreadl0ck/netcap/types"
@@ -71,9 +69,6 @@ import (
 )
 
 var (
-	defragger     = ip4defrag.NewIPv4Defragmenter()
-	streamFactory = &tcpConnectionFactory{}
-	StreamPool    = reassembly.NewStreamPool(streamFactory)
 	numErrors     uint
 
 	requests  = 0
@@ -86,7 +81,6 @@ var (
 	start          = time.Now()
 	errorsMap      = make(map[string]uint)
 	errorsMapMutex sync.Mutex
-	fsmOptions     = reassembly.TCPSimpleFSMOptions{}
 )
 
 var reassemblyStats struct {
@@ -121,160 +115,6 @@ func NumSavedConns() int64 {
 	statsMutex.Lock()
 	defer statsMutex.Unlock()
 	return reassemblyStats.savedConnections
-}
-
-/*
- * The TCP factory: returns a new Connection
- */
-
-type tcpConnectionFactory struct {
-	wg            sync.WaitGroup
-	decodeHTTP    bool
-	decodePOP3    bool
-	numActive     int64
-	streamReaders []StreamReader
-	sync.Mutex
-}
-
-type StreamReader interface {
-	ClientStream() []byte
-	ServerStream() []byte
-	ConversationRaw() []byte
-	ConversationColored() []byte
-	IsClient() bool
-	Ident() string
-	Network() gopacket.Flow
-	Transport() gopacket.Flow
-	FirstPacket() time.Time
-	Saved() bool
-}
-
-// New handles a new stream received from the assembler
-func (factory *tcpConnectionFactory) New(net, transport gopacket.Flow, tcp *layers.TCP, ac reassembly.AssemblerContext) reassembly.Stream {
-
-	logReassemblyDebug("* NEW: %s %s\n", net, transport)
-
-	stream := &tcpConnection{
-		net:         net,
-		transport:   transport,
-		isHTTP:      factory.decodeHTTP && (tcp.SrcPort == 80 || tcp.DstPort == 80),
-		isPOP3:      factory.decodePOP3 && (tcp.SrcPort == 110 || tcp.DstPort == 110),
-		isHTTPS:     tcp.SrcPort == 443 || tcp.DstPort == 443,
-		reversed:    tcp.SrcPort == 80,
-		tcpstate:    reassembly.NewTCPSimpleFSM(fsmOptions),
-		ident:       filepath.Clean(fmt.Sprintf("%s-%s", net, transport)),
-		optchecker:  reassembly.NewTCPOptionCheck(),
-		firstPacket: ac.GetCaptureInfo().Timestamp,
-	}
-
-	switch {
-	case stream.isHTTP:
-		stream.client = &httpReader{
-			bytes:    make(chan []byte, c.StreamDecoderBufSize),
-			ident:    filepath.Clean(fmt.Sprintf("%s-%s", net, transport)),
-			hexdump:  c.HexDump,
-			parent:   stream,
-			isClient: true,
-		}
-		stream.server = &httpReader{
-			bytes:   make(chan []byte, c.StreamDecoderBufSize),
-			ident:   filepath.Clean(fmt.Sprintf("%s-%s", net.Reverse(), transport.Reverse())),
-			hexdump: c.HexDump,
-			parent:  stream,
-		}
-
-		// kickoff decoders for client and server
-		factory.wg.Add(2)
-		factory.Lock()
-		factory.streamReaders = append(factory.streamReaders, stream.client.(StreamReader))
-		factory.streamReaders = append(factory.streamReaders, stream.server.(StreamReader))
-		factory.numActive += 2
-		factory.Unlock()
-		go stream.client.Run(factory)
-		go stream.server.Run(factory)
-
-	case stream.isPOP3:
-		stream.client = &pop3Reader{
-			bytes:    make(chan []byte, c.StreamDecoderBufSize),
-			ident:    filepath.Clean(fmt.Sprintf("%s-%s", net, transport)),
-			hexdump:  c.HexDump,
-			parent:   stream,
-			isClient: true,
-		}
-		stream.server = &pop3Reader{
-			bytes:   make(chan []byte, c.StreamDecoderBufSize),
-			ident:   filepath.Clean(fmt.Sprintf("%s-%s", net.Reverse(), transport.Reverse())),
-			hexdump: c.HexDump,
-			parent:  stream,
-		}
-
-		// kickoff decoders for client and server
-		factory.wg.Add(2)
-		factory.Lock()
-		factory.streamReaders = append(factory.streamReaders, stream.client.(StreamReader))
-		factory.streamReaders = append(factory.streamReaders, stream.server.(StreamReader))
-		factory.numActive += 2
-		factory.Unlock()
-		go stream.client.Run(factory)
-		go stream.server.Run(factory)
-	default:
-
-		// do not write encrypted HTTP streams to disk for now
-		if stream.isHTTPS {
-			// don't capture encrypted HTTPS traffic
-			return stream
-		}
-
-		if c.SaveStreams {
-
-			stream.client = &tcpReader{
-				bytes:    make(chan []byte, c.StreamDecoderBufSize),
-				ident:    filepath.Clean(fmt.Sprintf("%s-%s", net, transport)),
-				hexdump:  c.HexDump,
-				parent:   stream,
-				isClient: true,
-			}
-			stream.server = &tcpReader{
-				bytes:   make(chan []byte, c.StreamDecoderBufSize),
-				ident:   filepath.Clean(fmt.Sprintf("%s-%s", net.Reverse(), transport.Reverse())),
-				hexdump: c.HexDump,
-				parent:  stream,
-			}
-
-			// kickoff readers for client and server
-			factory.wg.Add(2)
-			factory.Lock()
-			factory.streamReaders = append(factory.streamReaders, stream.client.(StreamReader))
-			factory.streamReaders = append(factory.streamReaders, stream.server.(StreamReader))
-			factory.numActive += 2
-			factory.Unlock()
-			go stream.client.Run(factory)
-			go stream.server.Run(factory)
-		}
-	}
-
-	return stream
-}
-
-func (factory *tcpConnectionFactory) WaitGoRoutines() {
-
-	if !Quiet {
-		factory.Lock()
-		fmt.Println("\nwaiting for", factory.numActive, "flows")
-		factory.Unlock()
-	}
-
-	factory.wg.Wait()
-}
-
-// Context is the assembler context
-type Context struct {
-	CaptureInfo gopacket.CaptureInfo
-}
-
-// GetCaptureInfo returns the gopacket.CaptureInfo from the context
-func (c *Context) GetCaptureInfo() gopacket.CaptureInfo {
-	return c.CaptureInfo
 }
 
 /*
