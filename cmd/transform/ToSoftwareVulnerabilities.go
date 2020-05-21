5517e98b6c8ff06d5f8e94fd4235256b046d8aa4
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-05-21 16:47:47 +0200

    maltego iteration

--- cmd/transform/ToSoftwareVulnerabilities.go
+++ cmd/transform/ToSoftwareVulnerabilities.go
@@ -3,21 +3,29 @@ package transform
 import (
 	maltego "github.com/dreadl0ck/netcap/maltego"
 	"github.com/dreadl0ck/netcap/types"
+	"strings"
 )
 
 func ToSoftwareVulnerabilities() {
 	maltego.VulnerabilityTransform(
 		nil,
 		func(lt maltego.LocalTransform, trx *maltego.MaltegoTransform, vuln *types.Vulnerability, min, max uint64, profilesFile string, mac string, ipaddr string) {
-			ent := trx.AddEntity("netcap.Vulnerability", vuln.Description)
+
+			parts := strings.Split(vuln.Description, "-")
+			val := vuln.Description
+			if len(parts) > 1 {
+				val = parts[0] + "\n" + strings.Join(parts[1:], "-")
+			}
+			val = maltego.EscapeText(val)
+			ent := trx.AddEntity("netcap.Vulnerability", val)
 			ent.SetType("netcap.Vulnerability")
-			ent.SetValue(vuln.Description)
+			ent.SetValue(val)
 
 			ent.AddProperty("timestamp", "Timestamp", "strict", vuln.Timestamp)
 			ent.AddProperty("id", "ID", "strict", vuln.ID)
 			ent.AddProperty("file", "File", "strict", vuln.File)
-			ent.AddProperty("notes", "Notes", "strict", vuln.Notes)
-			ent.AddProperty("software", "Software", "strict", vuln.Software.Product + " " + vuln.Software.Version)
+			ent.AddProperty("notes", "Notes", "strict", maltego.EscapeText(vuln.Notes))
+			ent.AddProperty("software", "Software", "strict", maltego.EscapeText(vuln.Software.Product + " " + vuln.Software.Version))
 
 			ent.SetLinkColor("#000000")
 			//ent.SetLinkThickness(maltego.GetThickness(uint64(count), min, max))
--- cmd/transform/ToTCPServices.go
+++ cmd/transform/ToTCPServices.go
@@ -11,21 +11,38 @@ func ToTCPServices() {
 		nil,
 		func(lt maltego.LocalTransform, trx *maltego.MaltegoTransform, service *types.Service, min, max uint64, profilesFile string, mac string, ipaddr string) {
 			if service.Protocol == "TCP" {
-				ent := trx.AddEntity("netcap.TCPService", service.Hostname)
+
+				ent := trx.AddEntity("netcap.TCPService", service.IP + ":" + service.Port)
 				ent.SetType("netcap.TCPService")
-				ent.SetValue(service.IP + ":" + service.Port)
+
+				val := service.IP + ":" + service.Port
+				if len(service.Vendor) > 0 {
+					val += "\n" + service.Vendor
+				}
+				if len(service.Product) > 0 {
+					val += "\n" + service.Product
+				}
+				if len(service.Name) > 0 {
+					val += "\n" + service.Name
+				}
+				val = maltego.EscapeText(val)
+				ent.SetValue(val)
 
 				ent.AddProperty("timestamp", "Timestamp", "strict", service.Timestamp)
-				ent.AddProperty("product", "Product", "strict", service.Product)
-				ent.AddProperty("version", "Version", "strict", service.Version)
+				ent.AddProperty("product", "Product", "strict", maltego.EscapeText(service.Product))
+				ent.AddProperty("version", "Version", "strict", maltego.EscapeText(service.Version))
 				ent.AddProperty("protocol", "Protocol", "strict", service.Protocol)
 				ent.AddProperty("ip", "IP", "strict", service.IP)
 				ent.AddProperty("port", "Port", "strict", service.Port)
-				ent.AddProperty("hostname", "Hostname", "strict", service.Hostname)
+				ent.AddProperty("hostname", "Hostname", "strict", maltego.EscapeText(service.Hostname))
 				ent.AddProperty("bytesclient", "BytesClient", "strict", strconv.Itoa(int(service.BytesClient)))
 				ent.AddProperty("bytesserver", "BytesServer", "strict", strconv.Itoa(int(service.BytesServer)))
-				ent.AddProperty("vendor", "Vendor", "strict", service.Vendor)
-				ent.AddProperty("name", "Name", "strict", service.Name)
+				ent.AddProperty("vendor", "Vendor", "strict", maltego.EscapeText(service.Vendor))
+				ent.AddProperty("name", "Name", "strict", maltego.EscapeText(service.Name))
+
+				if len(service.Banner) > 0 {
+					ent.SetNote(maltego.EscapeText(string(service.Banner)))
+				}
 
 				ent.SetLinkColor("#000000")
 				//ent.SetLinkThickness(maltego.GetThickness(uint64(count), min, max))
