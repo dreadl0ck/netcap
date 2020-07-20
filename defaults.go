0d44b08845731cf297c017789016d427a06577a3
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-07-20 19:59:57 +0200

    reassembly: use the context from the first non empty byte container

--- defaults.go
+++ defaults.go
@@ -7,23 +7,23 @@ import (
 
 const (
 	// Size for memory buffering before feeding data into compressor
-	DefaultBufferSize           = 1024 * 1024 * 12 // 12 MB
+	DefaultBufferSize = 1024 * 1024 * 12 // 12 MB
 
 	// Size of the channel for feeding packets into workers
-	DefaultPacketBuffer         = 100
+	DefaultPacketBuffer = 100
 
 	// Default snap length for an ethernet frame:
 	// 1500 Ethernet MTU + 14 bytes Ethernet header
-	DefaultSnapLen              = 1514
+	DefaultSnapLen = 1514
 
 	// Settings for Flow and Connection audit record generation
 	// TODO: refactor to flush periodically, instead of every n packets?
-	DefaultConnFlushInterval    = 0
-	DefaultFlowFlushInterval    = 0
+	DefaultConnFlushInterval = 0
+	DefaultFlowFlushInterval = 0
 
 	// will be used to set age threshold if the corresponding FlushInterval > 0
-	DefaultConnTimeOut          = 0 * time.Second
-	DefaultFlowTimeOut          = 0 * time.Second
+	DefaultConnTimeOut = 0 * time.Second
+	DefaultFlowTimeOut = 0 * time.Second
 
 	// Compression Settings
 	DefaultCompressionBlockSize = 1024 * 1024 * 1 // 1 MB
@@ -34,14 +34,14 @@ const (
 	// in order to capture as much information as possible.
 
 	// How long to wait for remaining open streams to close, before initiating teardown
-	DefaultReassemblyTimeout    = 5 * time.Second
+	DefaultReassemblyTimeout = 5 * time.Second
 
 	// TODO: refactor to flush periodically, instead of every n packets?
 	// controls how often collected reassembly data is flushed to consumers
-	DefaultFlushEvery           = 100
+	DefaultFlushEvery = 100
 
 	// Close streams with pending bytes after
-	DefaultClosePendingTimeout  = 1 * time.Hour
+	DefaultClosePendingTimeout = 1 * time.Hour
 
 	// Close inactive streams after
 	DefaultCloseInactiveTimeout = 1 * time.Hour
--- encoder/tcpFactory.go
+++ encoder/tcpFactory.go
@@ -19,8 +19,6 @@ import (
 	"github.com/dreadl0ck/gopacket/ip4defrag"
 	"github.com/dreadl0ck/gopacket/layers"
 	"github.com/dreadl0ck/netcap/reassembly"
-	"time"
-
 	"path/filepath"
 	"sync"
 )
@@ -173,9 +171,4 @@ type Context struct {
 // GetCaptureInfo returns the gopacket.CaptureInfo from the context
 func (c *Context) GetCaptureInfo() gopacket.CaptureInfo {
 	return c.CaptureInfo
-}
-
-// GetCaptureInfo returns the gopacket.CaptureInfo from the context
-func (c *Context) SetTimestamp(t time.Time) {
-	c.CaptureInfo.Timestamp = t
 }
