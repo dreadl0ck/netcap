d6b30015ed1391525694af86f9bb45174f83d7cd
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-08-04 22:30:12 +0200

    refactored connection decoder

--- decoder/connection.go
+++ decoder/connection.go
@@ -25,6 +25,19 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
+// ConnectionID is a bidirectional connection
+// between two devices over the network
+// that includes the Link, Network and TransportLayer
+type ConnectionID struct {
+	LinkFlowID      uint64
+	NetworkFlowID   uint64
+	TransportFlowID uint64
+}
+
+func (c ConnectionID) String() string {
+	return strconv.FormatUint(c.LinkFlowID, 10) + strconv.FormatUint(c.NetworkFlowID, 10) + strconv.FormatUint(c.TransportFlowID, 10)
+}
+
 type Conn struct {
 	*types.Connection
 	sync.Mutex
@@ -44,180 +57,188 @@ func (a *AtomicConnMap) Size() int {
 	return len(a.Items)
 }
 
-var (
-	// Connections hold all connections
-	Connections = &AtomicConnMap{
-		Items: make(map[string]*Conn),
-	}
-	connDecoderInstance *CustomDecoder
-	conns               int64
-)
-
-// ConnectionID is a bidirectional connection
-// between two devices over the network
-// that includes the Link, Network and TransportLayer
-type ConnectionID struct {
-	LinkFlowID      uint64
-	NetworkFlowID   uint64
-	TransportFlowID uint64
+type ConnectionDecoder struct {
+	*CustomDecoder
+	Conns *AtomicConnMap
 }
 
-func (c ConnectionID) String() string {
-	return strconv.FormatUint(c.LinkFlowID, 10) + strconv.FormatUint(c.NetworkFlowID, 10) + strconv.FormatUint(c.TransportFlowID, 10)
-}
-
-var connectionDecoder = NewCustomDecoder(
-	types.Type_NC_Connection,
-	"Connection",
-	"A connection represents bi-directional network communication between two hosts based on the combined link-, network- and transport layer identifiers",
-	func(d *CustomDecoder) error {
-		connDecoderInstance = d
-		return nil
+var connectionDecoder = &ConnectionDecoder{
+	CustomDecoder: &CustomDecoder{
+		Type:        types.Type_NC_Connection,
+		Name:        "Connection",
+		Description: "A connection represents bi-directional network communication between two hosts based on the combined link-, network- and transport layer identifiers",
 	},
-	func(p gopacket.Packet) proto.Message {
+	Conns: &AtomicConnMap{
+		Items: make(map[string]*Conn),
+	},
+}
 
-		// assemble connectionID
-		connID := ConnectionID{}
-		if ll := p.LinkLayer(); ll != nil {
-			connID.LinkFlowID = ll.LinkFlow().FastHash()
-		}
-		if nl := p.NetworkLayer(); nl != nil {
-			connID.NetworkFlowID = nl.NetworkFlow().FastHash()
-		}
-		if tl := p.TransportLayer(); tl != nil {
-			connID.TransportFlowID = tl.TransportFlow().FastHash()
-		}
+func (cd *ConnectionDecoder) PostInit() error {
 
-		// lookup flow
-		Connections.Lock()
-		if conn, ok := Connections.Items[connID.String()]; ok {
+	// simply overwrite the handler with our custom one
+	// this way the CustomEncoders default Decode() implementation will be used
+	// (it takes care of applying config options and tracking stats)
+	// but with our custom logic to handle the actual packet
+	cd.Handler = cd.handlePacket
 
-			// connID exists. update fields
-			calcDuration := false
+	return nil
+}
 
-			conn.Lock()
+// Destroy closes and flushes all writers and calls deinit if set
+func (cd *ConnectionDecoder) Destroy() (name string, size int64) {
 
-			// check if received packet from the same flow
-			// was captured BEFORE the flows first seen timestamp
-			if !utils.StringToTime(conn.TimestampFirst).Before(p.Metadata().Timestamp) {
+	// call Deinit on FlowDecoder, instead of CustomDecoder
+	err := cd.DeInit()
+	if err != nil {
+		panic(err)
+	}
 
-				calcDuration = true
+	return cd.writer.Close()
+}
 
-				// rewrite timestamp
-				conn.TimestampFirst = utils.TimeToString(p.Metadata().Timestamp)
+func (cd *ConnectionDecoder) handlePacket(p gopacket.Packet) proto.Message {
+	// assemble connectionID
+	connID := ConnectionID{}
+	if ll := p.LinkLayer(); ll != nil {
+		connID.LinkFlowID = ll.LinkFlow().FastHash()
+	}
+	if nl := p.NetworkLayer(); nl != nil {
+		connID.NetworkFlowID = nl.NetworkFlow().FastHash()
+	}
+	if tl := p.TransportLayer(); tl != nil {
+		connID.TransportFlowID = tl.TransportFlow().FastHash()
+	}
 
-				// rewrite source and destination parameters
-				// since the first packet decides about the flow direction
-				if ll := p.LinkLayer(); ll != nil {
-					conn.SrcMAC = ll.LinkFlow().Src().String()
-					conn.DstMAC = ll.LinkFlow().Dst().String()
-				}
-				if nl := p.NetworkLayer(); nl != nil {
-					conn.SrcIP = nl.NetworkFlow().Src().String()
-					conn.DstIP = nl.NetworkFlow().Dst().String()
-				}
-				if tl := p.TransportLayer(); tl != nil {
-					conn.SrcPort = tl.TransportFlow().Src().String()
-					conn.DstPort = tl.TransportFlow().Dst().String()
-				}
-			}
+	// lookup flow
+	cd.Conns.Lock()
+	if conn, ok := cd.Conns.Items[connID.String()]; ok {
 
-			// check if last timestamp was before the current packet
-			if utils.StringToTime(conn.TimestampLast).Before(p.Metadata().Timestamp) {
-				// current packet is newer
-				// update last seen timestamp
-				conn.TimestampLast = utils.TimeToString(p.Metadata().Timestamp)
-				calcDuration = true
-			} // else: do nothing, timestamp is still the oldest one
+		// connID exists. update fields
+		calcDuration := false
 
-			conn.NumPackets++
-			conn.TotalSize += int32(len(p.Data()))
+		conn.Lock()
 
-			// only calculate duration when timetamps have changed
-			if calcDuration {
-				conn.Duration = utils.StringToTime(conn.TimestampLast).Sub(utils.StringToTime(conn.TimestampFirst)).Nanoseconds()
-			}
+		// check if received packet from the same flow
+		// was captured BEFORE the flows first seen timestamp
+		if !utils.StringToTime(conn.TimestampFirst).Before(p.Metadata().Timestamp) {
 
-			conn.Unlock()
-		} else {
+			calcDuration = true
 
-			// create a new Connection
-			conn := &types.Connection{}
-			conn.UID = calcMd5(conn.String())
+			// rewrite timestamp
 			conn.TimestampFirst = utils.TimeToString(p.Metadata().Timestamp)
 
+			// rewrite source and destination parameters
+			// since the first packet decides about the flow direction
 			if ll := p.LinkLayer(); ll != nil {
-				conn.LinkProto = ll.LayerType().String()
 				conn.SrcMAC = ll.LinkFlow().Src().String()
 				conn.DstMAC = ll.LinkFlow().Dst().String()
 			}
 			if nl := p.NetworkLayer(); nl != nil {
-				conn.NetworkProto = nl.LayerType().String()
 				conn.SrcIP = nl.NetworkFlow().Src().String()
 				conn.DstIP = nl.NetworkFlow().Dst().String()
 			}
 			if tl := p.TransportLayer(); tl != nil {
-				conn.TransportProto = tl.LayerType().String()
 				conn.SrcPort = tl.TransportFlow().Src().String()
 				conn.DstPort = tl.TransportFlow().Dst().String()
 			}
-			if al := p.ApplicationLayer(); al != nil {
-				conn.ApplicationProto = al.LayerType().String()
-				conn.AppPayloadSize = int32(len(al.Payload()))
-			}
-			Connections.Items[connID.String()] = &Conn{
-				Connection: conn,
-			}
+		}
 
-			stats.numConns++
+		// check if last timestamp was before the current packet
+		if utils.StringToTime(conn.TimestampLast).Before(p.Metadata().Timestamp) {
+			// current packet is newer
+			// update last seen timestamp
+			conn.TimestampLast = utils.TimeToString(p.Metadata().Timestamp)
+			calcDuration = true
+		} // else: do nothing, timestamp is still the oldest one
 
-			// flush
-			if c.ConnFlushInterval != 0 && conns%int64(c.ConnFlushInterval) == 0 {
+		conn.NumPackets++
+		conn.TotalSize += int32(len(p.Data()))
 
-				var selectConns []*types.Connection
-				for id, entry := range Connections.Items {
-					// flush entries whose last timestamp is connTimeOut older than current packet
-					if p.Metadata().Timestamp.Sub(utils.StringToTime(entry.TimestampLast)) > c.ConnTimeOut {
-						selectConns = append(selectConns, entry.Connection)
-						// cleanup
-						delete(Connections.Items, id)
-					}
+		// only calculate duration when timetamps have changed
+		if calcDuration {
+			conn.Duration = utils.StringToTime(conn.TimestampLast).Sub(utils.StringToTime(conn.TimestampFirst)).Nanoseconds()
+		}
+
+		conn.Unlock()
+	} else {
+
+		// create a new Connection
+		conn := &types.Connection{}
+		conn.UID = calcMd5(conn.String())
+		conn.TimestampFirst = utils.TimeToString(p.Metadata().Timestamp)
+
+		if ll := p.LinkLayer(); ll != nil {
+			conn.LinkProto = ll.LayerType().String()
+			conn.SrcMAC = ll.LinkFlow().Src().String()
+			conn.DstMAC = ll.LinkFlow().Dst().String()
+		}
+		if nl := p.NetworkLayer(); nl != nil {
+			conn.NetworkProto = nl.LayerType().String()
+			conn.SrcIP = nl.NetworkFlow().Src().String()
+			conn.DstIP = nl.NetworkFlow().Dst().String()
+		}
+		if tl := p.TransportLayer(); tl != nil {
+			conn.TransportProto = tl.LayerType().String()
+			conn.SrcPort = tl.TransportFlow().Src().String()
+			conn.DstPort = tl.TransportFlow().Dst().String()
+		}
+		if al := p.ApplicationLayer(); al != nil {
+			conn.ApplicationProto = al.LayerType().String()
+			conn.AppPayloadSize = int32(len(al.Payload()))
+		}
+		cd.Conns.Items[connID.String()] = &Conn{
+			Connection: conn,
+		}
+
+		conns := atomic.AddInt64(&stats.numConns, 1)
+
+		// flush
+		if c.ConnFlushInterval != 0 && conns%int64(c.ConnFlushInterval) == 0 {
+
+			var selectConns []*types.Connection
+			for id, entry := range cd.Conns.Items {
+				// flush entries whose last timestamp is connTimeOut older than current packet
+				if p.Metadata().Timestamp.Sub(utils.StringToTime(entry.TimestampLast)) > c.ConnTimeOut {
+					selectConns = append(selectConns, entry.Connection)
+					// cleanup
+					delete(cd.Conns.Items, id)
 				}
+			}
 
-				// flush selection in background
-				go func() {
-					for _, c := range selectConns {
-						writeConn(c)
-					}
-				}()
-			}
+			// flush selection in background
+			go func() {
+				for _, c := range selectConns {
+					cd.writeConn(c)
+				}
+			}()
 		}
-		Connections.Unlock()
-		return nil
-	},
-	func(e *CustomDecoder) error {
-		if !e.writer.IsChanWriter {
-			Connections.Lock()
-			for _, c := range Connections.Items {
-				c.Lock()
-				writeConn(c.Connection)
-				c.Unlock()
-			}
-			Connections.Unlock()
+	}
+	cd.Conns.Unlock()
+	return nil
+}
+
+func (cd *ConnectionDecoder) DeInit() error {
+	if !cd.writer.IsChanWriter {
+		cd.Conns.Lock()
+		for _, f := range cd.Conns.Items {
+			f.Lock()
+			cd.writeConn(f.Connection)
+			f.Unlock()
 		}
-		return nil
-	},
-)
+		cd.Conns.Unlock()
+	}
+	return nil
+}
 
 // writeConn writes the connection
-func writeConn(conn *types.Connection) {
+func (cd *ConnectionDecoder) writeConn(conn *types.Connection) {
 
 	if c.Export {
 		conn.Inc()
 	}
 
-	atomic.AddInt64(&connDecoderInstance.numRecords, 1)
-	err := connDecoderInstance.writer.Write(conn)
+	atomic.AddInt64(&cd.numRecords, 1)
+	err := cd.writer.Write(conn)
 	if err != nil {
 		log.Fatal("failed to write proto: ", err)
 	}
