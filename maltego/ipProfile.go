6d8cbdc8c17900f59975278e22a60e9afa2b8876
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-09-09 23:56:06 +0200

    bootstrapped transform over IP profiles

--- maltego/ipProfile.go
+++ maltego/ipProfile.go
@@ -15,6 +15,7 @@ package maltego
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -27,6 +28,119 @@ import (
 	netio "github.com/dreadl0ck/netcap/io"
 	"github.com/dreadl0ck/netcap/types"
 )
+
+// CountIPPackets returns the lowest and highest number of packets seen for a given IPProfile.
+var CountIPPackets = func(profile *types.IPProfile, mac string, min, max *uint64, _ map[string]*types.IPProfile) {
+	if uint64(profile.NumPackets) < *min {
+		*min = uint64(profile.NumPackets)
+	}
+	if uint64(profile.NumPackets) > *max {
+		*max = uint64(profile.NumPackets)
+	}
+}
+
+// countFunc is a function that counts something over IPProfiles.
+type ipProfileCountFunc = func(profile *types.IPProfile, mac string, min, max *uint64, ips map[string]*types.IPProfile)
+
+// IPProfileTransformationFunc is a transformation over IP profiles
+//goland:noinspection GoUnnecessarilyExportedIdentifiers
+type IPProfileTransformationFunc = func(lt LocalTransform, trx *Transform, profile *types.IPProfile, min, max uint64, path string, mac string, ip string)
+
+// IPProfileTransform applies a maltego transformation over IP profiles
+func IPProfileTransform(count ipProfileCountFunc, transform IPProfileTransformationFunc) {
+	var (
+		lt     = ParseLocalArguments(os.Args[1:])
+		path   = lt.Values["path"]
+		mac    = lt.Values["mac"]
+		ipaddr = lt.Values["ipaddr"]
+		trx    = Transform{}
+	)
+
+	netio.FPrintBuildInfo(os.Stderr)
+
+	f := openPath(path)
+
+	// check if its an audit record file
+	if !strings.HasSuffix(f.Name(), defaults.FileExtensionCompressed) && !strings.HasSuffix(f.Name(), defaults.FileExtension) {
+		die(errUnexpectedFileType, f.Name())
+	}
+
+	r := openNetcapArchive(path)
+
+	// read netcap header
+	header, errFileHeader := r.ReadHeader()
+	if errFileHeader != nil {
+		die("failed to read file header", errFileHeader.Error())
+	}
+
+	if header.Type != types.Type_NC_IPProfile {
+		die("file does not contain DeviceProfile records", header.Type.String())
+	}
+
+	var (
+		profile = new(types.IPProfile)
+		pm      proto.Message
+		ok      bool
+	)
+
+	pm = profile
+
+	if _, ok = pm.(types.AuditRecord); !ok {
+		panic("type does not implement types.AuditRecord interface")
+	}
+
+	var (
+		min      uint64 = 10000000
+		max      uint64 = 0
+		profiles        = LoadIPProfiles()
+		err      error
+	)
+
+	if count != nil {
+		for {
+			err = r.Next(profile)
+			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+				break
+			} else if err != nil {
+				die(err.Error(), errUnexpectedReadFailure)
+			}
+
+			count(profile, mac, &min, &max, profiles)
+		}
+
+		err = r.Close()
+		if err != nil {
+			log.Println("failed to close audit record file: ", err)
+		}
+	}
+
+	r, err = netio.Open(path, defaults.BufferSize)
+	if err != nil {
+		die(err.Error(), "failed to open file")
+	}
+
+	// read netcap header - ignore err as it has been checked before
+	_, _ = r.ReadHeader()
+
+	for {
+		err = r.Next(profile)
+		if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		transform(lt, &trx, profile, min, max, path, mac, ipaddr)
+	}
+
+	err = r.Close()
+	if err != nil {
+		log.Println("failed to close audit record file: ", err)
+	}
+
+	trx.AddUIMessage("completed!", UIMessageInform)
+	fmt.Println(trx.ReturnOutput())
+}
 
 // LoadIPProfiles will load the ipProfiles into memory and return them.
 func LoadIPProfiles() map[string]*types.IPProfile {
