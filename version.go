6622ff43057cdc8afbb79da33ef83b4f61a83726
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-09-16 18:02:18 +0200

    version bump

--- version.go
+++ version.go
@@ -2,7 +2,7 @@
 package netcap
 
 // Version is the current version identifier for netcap.
-var Version = "v0.5.3"
+var Version = "v0.5.4"
 
 // Commit is the git commit id of the current version.
-var Commit = "a086072c641a4f690e20ace55f0e3c2f10b3976c"
+var Commit = "9319e80ce6239f146f02917002a31b96b6951873"
