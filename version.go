e796a5a0e1850dab4881a583a633017892578dc2
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-09-20 18:06:10 +0200

    maltego testing and fixes

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.5.4"
 
 // Commit is the git commit id of the current version.
-var Commit = "73b09963b935e02791559acd16f0e3f4baea82b4"
+var Commit = "1aeef52e479645a2482b2e26a7ed9cd593692b7e"
