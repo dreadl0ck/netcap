f6c125824899fc0e9536b9ef8852fd42b592ef57
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-09-21 00:02:09 +0200

    ja3 hashes: track flows in maltego display information

--- cmd/transform/toJA3Hashes.go
+++ cmd/transform/toJA3Hashes.go
@@ -3,6 +3,7 @@ package transform
 import (
 	"github.com/dreadl0ck/netcap/maltego"
 	"github.com/dreadl0ck/netcap/types"
+	"github.com/dreadl0ck/netcap/utils"
 	"strconv"
 )
 
@@ -12,9 +13,10 @@ func toJA3Hashes() {
 		nil,
 		func(lt maltego.LocalTransform, trx *maltego.Transform, hello *types.TLSClientHello, min, max uint64, path string, ipaddr string) {
 
-			ent := trx.AddEntityWithPath("netcap.TLSClientHello", hello.Ja3+"\n"+hello.SrcIP+":"+strconv.Itoa(int(hello.SrcPort)), path)
+			ent := trx.AddEntityWithPath("netcap.TLSClientHello", hello.Ja3, path)
 			ent.AddProperty("ip", "IP", maltego.Strict, hello.SrcIP)
 			ent.AddProperty("port", "Port", maltego.Strict, strconv.Itoa(int(hello.SrcPort)))
+			ent.AddDisplayInformation(utils.CreateFlowIdent(hello.SrcIP, strconv.Itoa(int(hello.SrcPort)), hello.DstIP, strconv.Itoa(int(hello.DstPort))) + "<br>", "Flows")
 		},
 	)
 }
--- cmd/transform/toJA3SHashes.go
+++ cmd/transform/toJA3SHashes.go
@@ -3,6 +3,7 @@ package transform
 import (
 	"github.com/dreadl0ck/netcap/maltego"
 	"github.com/dreadl0ck/netcap/types"
+	"github.com/dreadl0ck/netcap/utils"
 	"strconv"
 )
 
@@ -12,9 +13,10 @@ func toJA3SHashes() {
 		nil,
 		func(lt maltego.LocalTransform, trx *maltego.Transform, hello *types.TLSServerHello, min, max uint64, path string, ipaddr string) {
 
-			ent := trx.AddEntityWithPath("netcap.TLSServerHello", hello.Ja3S+"\n"+hello.SrcIP+":"+strconv.Itoa(int(hello.SrcPort)), path)
+			ent := trx.AddEntityWithPath("netcap.TLSServerHello", hello.Ja3S, path)
 			ent.AddProperty("ip", "IP", maltego.Strict, hello.SrcIP)
 			ent.AddProperty("port", "Port", maltego.Strict, strconv.Itoa(int(hello.SrcPort)))
+			ent.AddDisplayInformation(utils.CreateFlowIdent(hello.SrcIP, strconv.Itoa(int(hello.SrcPort)), hello.DstIP, strconv.Itoa(int(hello.DstPort))) + "<br>", "Flows")
 		},
 	)
 }
