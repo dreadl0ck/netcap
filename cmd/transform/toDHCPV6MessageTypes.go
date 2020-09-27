750d641a73c81081ab96e720a4a852acebccd05f
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-09-27 22:55:28 +0200

    add dhcp v6 transforms

---
+++ cmd/transform/toDHCPV6MessageTypes.go
@@ -0,0 +1,40 @@
+package transform
+
+import (
+	"fmt"
+	"github.com/dreadl0ck/gopacket/layers"
+	"strconv"
+
+	"github.com/dreadl0ck/netcap/maltego"
+	"github.com/dreadl0ck/netcap/types"
+)
+
+func toDHCPV6MessageTypes() {
+	var (
+		msgTypes = make(map[int32]int)
+		pathName string
+	)
+
+	maltego.DHCPV6Transform(
+		nil,
+		func(lt maltego.LocalTransform, trx *maltego.Transform, d *types.DHCPv6, min, max uint64, path string, mac string) {
+			if path == "" {
+				pathName = path
+			}
+			msgTypes[d.MsgType]++
+		},
+		true,
+	)
+
+	trx := maltego.Transform{}
+	for val, numHits := range msgTypes {
+		ent := trx.AddEntityWithPath("netcap.DHCPv6MessageType", layers.DHCPv6MsgType(byte(val)).String(), pathName)
+		ent.AddProperty("value", "Value", maltego.Strict, strconv.Itoa(int(val)))
+		ent.SetLinkLabel(strconv.Itoa(numHits))
+	}
+
+	trx.AddUIMessage("completed!", maltego.UIMessageInform)
+	fmt.Println(trx.ReturnOutput())
+}
+
+
---
+++ cmd/transform/toDHCPV6Options.go
@@ -0,0 +1,42 @@
+package transform
+
+import (
+	"fmt"
+	"github.com/dreadl0ck/gopacket/layers"
+	"strconv"
+
+	"github.com/dreadl0ck/netcap/maltego"
+	"github.com/dreadl0ck/netcap/types"
+)
+
+func toDHCPV6Options() {
+	var (
+		opts = make(map[int32]int)
+		pathName string
+	)
+
+	maltego.DHCPV6Transform(
+		nil,
+		func(lt maltego.LocalTransform, trx *maltego.Transform, d *types.DHCPv6, min, max uint64, path string, mac string) {
+			if path == "" {
+				pathName = path
+			}
+			for _, o := range d.Options {
+				opts[o.Code]++
+			}
+		},
+		true,
+	)
+
+	trx := maltego.Transform{}
+	for val, numHits := range opts {
+		ent := trx.AddEntityWithPath("netcap.DHCPv6Option", layers.DHCPv6Opt(uint16(val)).String(), pathName)
+		ent.AddProperty("value", "Value", maltego.Strict, strconv.Itoa(int(val)))
+		ent.SetLinkLabel(strconv.Itoa(numHits))
+	}
+
+	trx.AddUIMessage("completed!", maltego.UIMessageInform)
+	fmt.Println(trx.ReturnOutput())
+}
+
+
