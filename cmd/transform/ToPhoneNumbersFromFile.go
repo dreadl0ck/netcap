c33816fc89cd7a46f08daac5fa7e2830ad19a3c3
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-01 14:47:12 +0200

    handle file prefix for other transforms

--- cmd/transform/ToPhoneNumbersFromFile.go
+++ cmd/transform/ToPhoneNumbersFromFile.go
@@ -18,7 +18,7 @@ func toPhoneNumbersFromFile() {
 	var (
 		lt   = maltego.ParseLocalArguments(os.Args)
 		trx  = &maltego.Transform{}
-		path = lt.Values["location"]
+		path = strings.TrimPrefix(lt.Values["location"], "file://")
 		err  error
 	)
 
