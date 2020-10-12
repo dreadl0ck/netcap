8f063afa0a41a1e5c2860e556ae8a038755e61ab
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-10-12 12:27:03 +0200

    debian install

--- zeus/scripts/install-debian.sh
+++ zeus/scripts/install-debian.sh
@@ -5,29 +5,29 @@
 sudo apt update
 sudo apt install -y libpcap-dev software-properties-common ca-certificates liblzo2-2 libkeyutils-dev
 sudo update-ca-certificates
-sudo apt-get install -y apt-transport-https curl lsb-release wget autogen autoconf libtool gcc libpcap-dev linux-headers-generic git vim autoconf automake libtool make g++ bison flex cmake binutils binutils-doc gcc-doc cmake-doc extra-cmake-modules
+sudo apt-get install -y apt-transport-https curl lsb-release wget autogen autoconf libtool gcc libpcap-dev linux-headers-generic git vim automake libtool make g++ bison flex cmake binutils binutils-doc gcc-doc cmake-doc extra-cmake-modules autoconf pkg-config libconfig-dev
 
-wget https://github.com/wanduow/wandio/archive/4.2.2-1.tar.gz
+wget -qN https://github.com/wanduow/wandio/archive/4.2.2-1.tar.gz
 tar xfz 4.2.2-1.tar.gz
 cd wandio-4.2.2-1 && ./bootstrap.sh && ./configure && make && sudo make install
 cd ..
 
-wget https://github.com/LibtraceTeam/libtrace/archive/4.0.11-1.tar.gz
+wget -qN https://github.com/LibtraceTeam/libtrace/archive/4.0.11-1.tar.gz
 tar xfz 4.0.11-1.tar.gz
 cd libtrace-4.0.11-1 && ./bootstrap.sh && ./configure && make && sudo make install
 cd ..
 
-wget https://github.com/wanduow/libflowmanager/archive/3.0.0.tar.gz
+wget -qN https://github.com/wanduow/libflowmanager/archive/3.0.0.tar.gz
 tar xfz 3.0.0.tar.gz
 cd libflowmanager-3.0.0 && ./bootstrap.sh && ./configure && make && sudo make install
 cd ..
 
-wget https://github.com/wanduow/libprotoident/archive/2.0.14-1.tar.gz
+wget -qN https://github.com/wanduow/libprotoident/archive/2.0.14-1.tar.gz
 tar xfz 2.0.14-1.tar.gz
 cd libprotoident-2.0.14-1 && ./bootstrap.sh && ./configure && make && sudo make install
 cd ..
 
-wget https://github.com/ntop/nDPI/archive/3.2.tar.gz
+wget -qN https://github.com/ntop/nDPI/archive/3.2.tar.gz
 tar xfz 3.2.tar.gz
 cd nDPI-3.2 && ./autogen.sh && ./configure && make && sudo make install
 cd ..
@@ -47,9 +47,9 @@ sudo ldconfig /usr/local/lib/*
 sudo ldconfig /go/*
 
 env
-#find / -iname ndpi_main.h
-#find / -iname libprotoident.h
-#find / -iname libtrace.h
+find / -iname ndpi_main.h
+find / -iname libprotoident.h
+find / -iname libtrace.h
 
 go build -mod=readonly -ldflags "-s -w -X github.com/dreadl0ck/netcap.Version=v${VERSION}" -o /usr/local/bin/net -i github.com/dreadl0ck/netcap/cmd
 
