2327bfe0c5911075709395cedbcf3e0f67ba1c4b
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-11-18 23:18:50 +0100

    shell out because go git does not support lfs

--- cmd/util/cloneDBs.go
+++ cmd/util/cloneDBs.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"errors"
 	"fmt"
 	"github.com/dreadl0ck/netcap/defaults"
 	"github.com/dreadl0ck/netcap/resolvers"
@@ -9,7 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"github.com/go-git/go-git/v5"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -19,39 +18,35 @@ func cloneDBs() {
 
 	// check if database root path exists already
 	if _, err := os.Stat(resolvers.ConfigRootPath); err == nil {
-		log.Fatal("database root path", resolvers.ConfigRootPath, "exists already")
+		log.Fatal("database root path: ", resolvers.ConfigRootPath, " exists already")
 	}
 
 	// it does not - create it
 	_ = os.MkdirAll(resolvers.ConfigRootPath, defaults.DirectoryPermission)
 
-	// change into dir
-	//err := os.Chdir(resolvers.DataBaseRootPath)
-	//if err != nil {
-	//	log.Fatal("failed to move into", resolvers.DataBaseRootPath, err)
-	//}
-
 	// clone repo
-	_, err := git.PlainClone(resolvers.ConfigRootPath, false, &git.CloneOptions{
-		URL:      "https://github.com/dreadl0ck/netcap-dbs.git",
-		Progress: os.Stdout,
-	})
-	if err != nil && !errors.Is(err, git.ErrRepositoryAlreadyExists) {
-		log.Fatal("failed to clone netcap-dbs repository:", err)
+	// go-git does not yet support LFS... we need to shell out
+	//_, err := git.PlainClone(resolvers.ConfigRootPath, false, &git.CloneOptions{
+	//	URL:      "https://github.com/dreadl0ck/netcap-dbs.git",
+	//	Progress: os.Stdout,
+	//})
+	//if err != nil && !errors.Is(err, git.ErrRepositoryAlreadyExists) {
+	//	log.Fatal("failed to clone netcap-dbs repository:", err)
+	//}
+	cmd := exec.Command("git", "clone", "https://github.com/dreadl0ck/netcap-dbs.git", resolvers.ConfigRootPath)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal("failed to clone netcap-dbs repo: ", err)
 	}
 
 	fmt.Println("cloned netcap-dbs repository to", resolvers.ConfigRootPath)
 
-	// rename? TODO
-	//err = os.Rename(filepath.Join(dir, "netcap-dbs"), filepath.Join(dir, "netcap"))
-	//if err != nil {
-	//	log.Fatal("failed to rename", dir, err)
-	//}
-
 	// decompress bleve stores
 	files, err := ioutil.ReadDir(resolvers.DataBaseFolderPath)
 	if err != nil {
-		log.Fatal("failed to read dir", resolvers.DataBaseFolderPath, err)
+		log.Fatal("failed to read dir: ", resolvers.DataBaseFolderPath, err)
 	}
 
 	for _, f := range files {
@@ -65,7 +60,7 @@ func cloneDBs() {
 				),
 			)
 			if err != nil {
-				log.Fatal("failed to unzip", f.Name(), err)
+				log.Fatal("failed to unzip: ", f.Name(), " error: ", err)
 			}
 		}
 	}
