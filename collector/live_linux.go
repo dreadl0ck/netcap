0ba3373f04a4b828edd8e04e3353c36ff96b37f0
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-06-07 01:11:41 +0200

    fix build on linux

--- collector/live_linux.go
+++ collector/live_linux.go
@@ -71,7 +71,7 @@ func (c *Collector) CollectLive(i string, bpf string) error {
 			if errors.Is(err, io.EOF) {
 				break
 			}
-			return errors.Wrap(err, errReadingPacketData + " file: " + path))
+			return errors.Wrap(err, errReadingPacketData + " file: " + path)
 		}
 
 		// increment atomic packet counter
