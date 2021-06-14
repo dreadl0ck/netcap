5ffa7af17c30ccfea3e22091c9749453742a1619
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-06-14 19:08:34 +0200

    refactor analyzer cli interface

---
+++ analyze/config.go
@@ -0,0 +1,33 @@
+package analyze
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+// Config structure for analyzer plugins.
+type Config struct {
+	WorkDir string `yaml:"workDir"`
+	Command string `yaml:"command"`
+	Args []string `yaml:"args"`
+
+	// TODO: handle
+	AuditRecords []string `yaml:"auditRecords"`
+}
+
+// ParseConfig parses and returns a config instance.
+func ParseConfig(path string) *Config {
+	var conf = new(Config)
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.UnmarshalStrict(contents, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conf
+}
