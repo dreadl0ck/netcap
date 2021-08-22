b0c210fe4d4ebfd23bb09c0082fcfc5888c0a8ad
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-08-22 16:07:53 +0200

    chore: upload assets manually to github release page

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.6.3"
 
 // Commit is the git commit id of the current version.
-var Commit = "fb5eb6605cb56b3d45de3f31552d4a9643f7f37e"
+var Commit = "daa5b43f538b3996ab905ea34e045c8da6dd041a"
