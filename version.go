fb5eb6605cb56b3d45de3f31552d4a9643f7f37e
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-08-22 15:28:58 +0200

    chore: release script updates

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.6.2"
 
 // Commit is the git commit id of the current version.
-var Commit = "2f7bd16d983b872a82af2ec8f6e14bef297830b8"
+var Commit = "3eaedc9ad368f2497ef0237b155d9621b58d8d9e"
