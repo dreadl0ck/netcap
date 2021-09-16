f6e0a6c763d158313f6284c523881689feebc448
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-09-16 22:30:51 +0200

    v0.6.6 release

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.6.6"
 
 // Commit is the git commit id of the current version.
-var Commit = "0d23954695434ff58a35aa276cb70f86d115903f"
+var Commit = "457f4365febba4812dfa9146ebe710cbc1f98a4c"
