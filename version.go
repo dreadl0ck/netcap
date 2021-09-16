5bb2c5acd3d1a2ccf7fa6aa04f08385041457e54
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-09-17 00:48:10 +0200

    chore: add publish-module command

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.6.6"
 
 // Commit is the git commit id of the current version.
-var Commit = "62b421be7bca185ad90e48d4afc6741a9dae7237"
+var Commit = "4204b12a12dd32bc091e148d070dde05aaf4f46c"
