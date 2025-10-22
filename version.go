33bcec4c42702e61f5bcff5b1b4ae243ee5e7190
Philipp Mieden <dreadl0ck@protonmail.ch>
2025-10-23 00:29:14 +0200

    fix: pkg-config

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.7.0"
 
 // Commit is the git commit id of the current version.
-var Commit = "404095cd9d29bf453e1cddfb524508d2579c114c"
+var Commit = "257368975da0ad969dacbb7ba17c308d89249677"
