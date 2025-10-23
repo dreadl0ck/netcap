27874412f6f72b33caa3b6f3cd9d9eb9b4096994
Philipp Mieden <dreadl0ck@protonmail.ch>
2025-10-23 04:58:30 +0200

    fix: error in release command

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.7.0"
 
 // Commit is the git commit id of the current version.
-var Commit = "33bcec4c42702e61f5bcff5b1b4ae243ee5e7190"
+var Commit = "2fa76f2a91145b743e21ab51a1424a11877cf704"
