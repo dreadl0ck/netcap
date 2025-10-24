b190550097fbb10cc912770b9c5767ecac84fc1b
Philipp Mieden <dreadl0ck@protonmail.ch>
2025-10-24 17:07:23 +0200

    fix dpi version info for builds distributed via homebrew, add usage info for mac users to release info

--- version.go
+++ version.go
@@ -5,4 +5,4 @@ package netcap
 var Version = "v0.7.3"
 
 // Commit is the git commit id of the current version.
-var Commit = "3ba9b66af0994e71023735e3fb8fa3218859e3d7"
+var Commit = "702383c6a1f1191e194b9913ba935f33115fc927"
