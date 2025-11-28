e976d1c5e07526bd6e1347cb040376ed407abbb5
Philipp Mieden <dreadl0ck@protonmail.ch>
2025-11-28 16:48:38 +0100

    move callvis graphics

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ package netcap
 var Version = "vlatest"
 
 // Commit is the git commit id of the current version.
-var Commit = "b72217fb50fbac1a88bc7fff96c9f5b7219dc01e"
+var Commit = "f84d77f48b878f0b287bf499028f058576afc73c"
 
 // GopacketVersion is the version of gopacket library used.
 var GopacketVersion = "v1.5.0"
