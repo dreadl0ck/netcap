a20c6e52555b34ae8a27f7ef4de6a08404797967
Philipp Mieden <dreadl0ck@protonmail.ch>
2026-09-20 23:34:47 +0200

    Harden Linux release builds

--- zeus/scripts/build-linux-nodpi.sh
+++ zeus/scripts/build-linux-nodpi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # musl
 NODPI=true ARGS="--no-cache --build-arg TAGS=-tags=nodpi,noyara" VERSION=nodpi-v${VERSION} zeus/scripts/build-alpine-docker.sh
 
--- zeus/scripts/build-linux.sh
+++ zeus/scripts/build-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # musl
 NODPI=false VERSION=v${VERSION} zeus/scripts/build-alpine-docker.sh
 
