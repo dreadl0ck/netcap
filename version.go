9e91ddc62ceb5bd4176f460fa09b2b33f5a22642
Philipp Mieden <dreadl0ck@protonmail.ch>
2026-09-25 01:49:42 +0400

    v0.9.13 release

--- version.go
+++ version.go
@@ -2,10 +2,10 @@
 package netcap
 
 // Version is the current version identifier for netcap.
-var Version = "v0.9.12"
+var Version = "v0.9.13"
 
 // Commit is the git commit id of the current version.
-var Commit = "28013de4a8ed2dc38db59e7e289cdfd6c19c6b77"
+var Commit = "82c5a8480db6b8d2e60e327770f4098f4562c15d"
 
 // GopacketVersion is the version of gopacket library used.
 var GopacketVersion = "v1.7.1"
